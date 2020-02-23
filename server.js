96c4d10d40def5ad5707951df0944d9a27851a32
Prakash Padhi <sriprakashchpadhis@gmail.com>
2020-02-23 12:21:15 +0530

    CSS Changes

--- server.js
+++ server.js
@@ -20,13 +20,10 @@ app.use("/api/users", require("./routes/api/users/allUsers"));
 app.use("/api/token", require("./routes/api/users/accessToken"));
 
 // Static assets in production
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
