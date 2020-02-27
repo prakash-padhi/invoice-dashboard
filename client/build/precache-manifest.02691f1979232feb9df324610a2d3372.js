9b6cee4806f11f959c505a869a107330d039e1b9
Prakash@DigitalOcean <sriprakashchpadhis@gmail.com>
2020-02-27 12:37:22 +0000

    Server Setup SuccessFully

---
+++ client/build/precache-manifest.02691f1979232feb9df324610a2d3372.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9bb6d19f007d5c81e0b10b0ad0beaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c884bc977b5e438f11e5",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "61c936d997800487400b",
+    "url": "/static/css/main.4b4ed85c.chunk.css"
+  },
+  {
+    "revision": "c884bc977b5e438f11e5",
+    "url": "/static/js/2.ce2990d5.chunk.js"
+  },
+  {
+    "revision": "86a572c8d774fa2516fae6aa5c728e23",
+    "url": "/static/js/2.ce2990d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c936d997800487400b",
+    "url": "/static/js/main.b34e6470.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "947bb1a25b01882c2a6a9799242e633b",
+    "url": "/static/media/404.947bb1a2.png"
+  },
+  {
+    "revision": "d4f498023801dd6ec85ab70468df2c03",
+    "url": "/static/media/AliensAndCows.d4f49802.ttf"
+  },
+  {
+    "revision": "ea2c3cf1be388bd3fbe9d0cd8afee11c",
+    "url": "/static/media/Invoicely400.ea2c3cf1.woff2"
+  },
+  {
+    "revision": "d4ff90db5da894c833f356f47a16e408",
+    "url": "/static/media/Invoicely500.d4ff90db.woff2"
+  },
+  {
+    "revision": "e409b7c96b77431dc079c1be861cbd84",
+    "url": "/static/media/add.e409b7c9.svg"
+  },
+  {
+    "revision": "48f0ed5c204e3289223e9e01ff8a1daf",
+    "url": "/static/media/logo192.48f0ed5c.png"
+  },
+  {
+    "revision": "37bbf1f91fee551dddeaf65332bbfc57",
+    "url": "/static/media/sad.37bbf1f9.svg"
+  }
+]);
